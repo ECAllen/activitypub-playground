fb83af3c55b89a81ba5aae749446229efd206ad6
Ethan Allen <ethancallen@gmail.com>
2020-04-18 16:53:07 -0400

    updates to client and server

--- cmd/web/client/main.go
+++ cmd/web/client/main.go
@@ -1,30 +1,46 @@
 package main
-`
+
 import (
-	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 )
 
 func main() {
-	url := "http://localhost:4000/actor"
-	fmt.Println("URL:>", url)
 
-	var jsonStr = []byte(`{"title":"Buy cheese and bread for breakfast."}`)
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
-	req.Header.Set("X-Custom-Header", "myvalue")
-	req.Header.Set("Content-Type", "application/json")
+	url := "http://localhost:4000/actors"
 
-	client := &http.Client{}
-	resp, err := client.Do(req)
+	/*
+		fmt.Println("URL:>", url)
+
+		var jsonStr = []byte(`{"title":"Buy cheese and bread for breakfast."}`)
+		req, err := http.NewRequest("GET", url)
+		req.Header.Set("X-Custom-Header", "myvalue")
+		req.Header.Set("Content-Type", "application/json")
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			panic(err)
+		}
+	*/
+
+	resp, err := http.Get(url)
 	if err != nil {
-		panic(err)
+		log.Fatal("%v\n", err)
 	}
 	defer resp.Body.Close()
 
 	fmt.Println("response Status:", resp.Status)
 	fmt.Println("response Headers:", resp.Header)
-	body, _ := ioutil.ReadAll(resp.Bod)
+	if resp.StatusCode != http.StatusOK {
+		log.Fatal("Not OK, exiting...")
+	}
+
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		log.Fatal("%v\n", err)
+	}
 	fmt.Println("response Body:", string(body))
 }
--- cmd/web/server/main.go
+++ cmd/web/server/main.go
@@ -1,23 +1,69 @@
 package main
 
 import (
+	"crypto/rsa"
+	"crypto/sha1"
 	"encoding/json"
-	"github.com/go-ap/activitypub"
+	"fmt"
+	pub "github.com/go-ap/activitypub"
 	"github.com/sirupsen/logrus"
+	"math/rand"
 	"net/http"
 	"os"
+)
+
+var (
+	UserAgent    = "ap-go-http-client"
+	HeaderAccept = `application/ld+json; profile="https://www.w3.org/ns/activitystreams"`
+
+	host            = "127.0.0.1:4000"
+	apiURL          = "http://127.0.0.1:4000"
+	authCallbackURL = fmt.Sprintf("%s/auth/local/callback", apiURL)
+
+	rnd    = rand.New(rand.NewSource(6667))
+	key, _ = rsa.GenerateKey(rnd, 512)
+
+	logger = logrus.New()
 )
 
 func inbox(w http.ResponseWriter, r *http.Request) {
 	logger.WithFields(logrus.Fields{"event": "Inbox"}).Trace("Inbox hit")
 }
 
-func actor(w http.ResponseWriter, r *http.Request) {
+func actors(w http.ResponseWriter, r *http.Request) {
 	logger.WithFields(logrus.Fields{"event": "Actor"}).Trace("Actor hit")
+	// TODO
+
+	/*
+		get list of actors
+		json marshall actors
+		respond with json
+		items: map[string]*objectVal{
+									"e869bdca-dd5e-4de7-9c5d-37845eccc6a1": {
+										id:      "http://127.0.0.1:9998/actors/e869bdca-dd5e-4de7-9c5d-37845eccc6a1",
+										typ:     string(pub.PersonType),
+										summary: "Generated actor",
+										content: "Generated actor",
+										url:     "http://127.0.0.1:9998/actors/e869bdca-dd5e-4de7-9c5d-37845eccc6a1",
+										inbox: &objectVal{
+											id: "http://127.0.0.1:9998/actors/e869bdca-dd5e-4de7-9c5d-37845eccc6a1/inbox",
+										},
+										outbox: &objectVal{
+											id: "http://127.0.0.1:9998/actors/e869bdca-dd5e-4de7-9c5d-37845eccc6a1/outbox",
+										},
+										liked: &objectVal{
+											id: "http://127.0.0.1:9998/actors/e869bdca-dd5e-4de7-9c5d-37845eccc6a1/liked",
+										},
+										preferredUsername: "johndoe",
+										name:              "Johnathan Doe",
+									},
+								},
+	*/
+
 }
 
 func handlerReqs() {
-	http.HandleFunc("/actor", actor)
+	http.HandleFunc("/actors", actors)
 	http.HandleFunc("/inbox", inbox)
 	logger.WithFields(logrus.Fields{"event": "Handle Requests"}).Trace("Handle Reqs")
 	err := http.ListenAndServe(":4000", nil)
@@ -29,17 +75,44 @@ func handlerReqs() {
 	}
 }
 
-var logger = logrus.New()
+type Actors struct {
+	Total  int
+	People []*pub.Person
+}
+
+func (a *Actors) Add(person *pub.Person) {
+	a.Total++
+	a.People = append(a.People, person)
+	return
+}
 
 func main() {
 	logger.Formatter = new(logrus.JSONFormatter)
 	logger.Level = logrus.TraceLevel
 	logger.Out = os.Stdout
 
-	id := activitypub.IRI("http://localhost:4000/actor")
-	a := activitypub.PersonNew(id)
-	aj, _ := json.MarshalIndent(a, "", "  ")
+	actorName := "Alice"
+	h := sha1.New()
+	h.Write([]byte(actorName))
+	actorSumHex := h.Sum(nil)
+	actorSum := fmt.Sprintf("%x", actorSumHex)
 
+	actorsURL := fmt.Sprintf("%s/actors", apiURL)
+	actorURL := fmt.Sprintf("%s/%s", actorsURL, actorSum)
+
+	logger.WithFields(logrus.Fields{
+		"event":      "SHA1 Sum",
+		"Actor Name": actorName,
+		"Actor Sum":  actorSum,
+		"Actor URL":  actorURL,
+	}).Trace("Actor Sum")
+
+	actors := Actors{}
+	id := pub.IRI(actorURL)
+	a := pub.PersonNew(id)
+	actors.Add(a)
+
+	aj, _ := json.MarshalIndent(a, "", "  ")
 	logger.WithFields(logrus.Fields{
 		"event": "Create",
 		"actor": string(aj),
@@ -50,15 +123,21 @@ func main() {
 
 /* TODO
 
-- create server
+# Server
+-  create server
 -  create endpoints
 -  listen on endpoints
-key handling???
-  receive message and put in inbox
 
-create client
-  publickey
-  send message
+respond with list of Actors to client
+receive message and put in inbox
+
+# Client
+
+- create client
+client gets list of actors
+client get inbox endpoint of actor
+publickey
+send message
 
 test by sending message to server
 */
